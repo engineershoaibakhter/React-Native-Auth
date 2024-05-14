8d0cce3479fab5c2e1807dde9843b00c7edef419
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-14 19:09:42 +0500

    Home Auth screen is completed

--- MyApp/app/(tabs)/index.tsx
+++ MyApp/app/(tabs)/index.tsx
@@ -6,11 +6,59 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <>
-    
+    <ThemedView style={styles.container}>
+      <Image style={styles.bgImage} source={require('../../assets/images/bg-auth-image.png')}></Image>
+      <View style={styles.textContent}>
+        <ThemedText style={styles.textLogin}>Login</ThemedText>
+        <ThemedText style={styles.textRegister}>Register</ThemedText>
+      </View>
+    </ThemedView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container:{
+    // marginTop:50
+    flex: 1,
+  },
+  bgImage:{
+    resizeMode: 'cover',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  textContent: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  textLogin: {
+    fontSize: 24,
+    fontWeight: "400",
+    color: 'white',
+    marginBottom: 20, // Space between Login and Register
+    backgroundColor: 'black',
+    width: '80%',
+    height: 50, // Adjust height according to your requirement
+    borderRadius: 10,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  textRegister:{
+    fontSize: 24,
+    fontWeight: "400",
+    color: 'black',
+    marginBottom: 20, // Space between Login and Register
+    backgroundColor: 'white',
+    width: '80%',
+    height: 50, // Adjust height according to your requirement
+    borderRadius: 10,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderColor:"black",
+    borderWidth: 2,
+  }
 });
