bc2d01fc2edb5ba36a88fd2a42a58388cb94e977
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-14 18:27:37 +0500

    setup is completed

---
+++ MyApp/app/(tabs)/explore.tsx
@@ -0,0 +1,30 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+
+import { ExternalLink } from '@/components/ExternalLink';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <>
+    <ThemedView>
+      <ThemedText>SHoaib</ThemedText>
+    </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
---
+++ MyApp/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet,View,Text, Platform } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
