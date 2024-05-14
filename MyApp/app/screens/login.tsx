bc564d0d66111f07161282c74215d113e4f6ce1e
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-14 20:31:14 +0500

    login page is continuing

--- MyApp/app/screens/login.tsx
+++ MyApp/app/screens/login.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 const login = () => {
   return (
-    <View>
-      <Text>login</Text>
-    </View>
-  )
-}
+    <ScrollView indicatorStyle={"black"} style={{ padding: 10 }}>
+      <View style={styles.loginContainer}>
+        <View style={styles.loginHeading}>
+          <Text style={styles.loginHeadingContent}>
+            Welcome back! We've missed your presence here
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
-export default login
+export default login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+  },
+  loginHeading: {
+    marginTop: 100,
+  },
+  loginHeadingContent: {
+    fontSize: 30,
+    fontWeight: "500",
+  },
+});
