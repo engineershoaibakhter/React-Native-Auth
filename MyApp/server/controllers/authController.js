12f7638a77fce38dddb418c559d4721af5e8f8e8
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-15 18:15:10 +0500

    authController file is created

--- MyApp/server/controllers/authController.js
+++ MyApp/server/controllers/authController.js
@@ -26,6 +26,30 @@ exports.register= async (req,res)=>{
         res.status(201).json({token});
     } catch (error) {
         res.status(500).json({message:'Server Error'});
-        console.log(`Server Error because of ${error}`);
+        console.log(`Server Error and Registration is not running because of ${error}`);
+    }
+}
+
+exports.login=async (req,res)=>{
+    const {email,password}=req.body;
+
+    try {
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        const token=jwt.sign({userId:user._id},secretKey,{
+            expiresIn:'6d',
+        });
+
+        res.json({token});
+    } catch (error) {
+        res.status(500).json({message:`Server Error and Login is not running becuase of ${error}`});
     }
 }
