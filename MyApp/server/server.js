e44712998578d05c292daa4e8312a27441af9f71
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-15 16:59:57 +0500

    server file is created and add code

---
+++ MyApp/server/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const cors= require('cors');
+
+dotenv.config();
+
+const app=express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to Mongodb
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("Mongodb is connected"))
+.catch((error)=>console.error("Mongodb is not connected because of "+error))
+
+// Routes
+app.use('/api/auth',require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`Server is running on PORT ${PORT}`));
+
+
