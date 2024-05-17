8178c3f6cd422b4d42e1fdbf933d882e54796369
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-17 17:03:06 +0500

    wrote otp and expire date in mongodb model

--- MyApp/server/models/User.js
+++ MyApp/server/models/User.js
@@ -5,6 +5,8 @@ const UserSchema=new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
+    otp:{type:String},
+    otpExpires:{type:Date},
 });
 
 UserSchema.pre('save',async (next)=>{
